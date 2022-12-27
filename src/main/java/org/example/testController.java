5601653b05466ec6d24c9ad21e823062567fd208
Arun Mohan <arun.mohan@nordencommunication.com>
2022-12-27 07:25:00 +0000

    tranist

--- src/main/java/org/example/testController.java
+++ src/main/java/org/example/testController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
 import reactor.core.publisher.Sinks;
 
+import java.time.Duration;
+
 /**
  * ----------------------*    Norden Communication    *-------------------------
  * Created on 26/12/2022
@@ -26,16 +28,33 @@ public class testController {
     @RequestMapping(method= RequestMethod.POST,value="/panic/id")
     public ResponseEntity<Boolean> mapp(@RequestBody Event event) throws Exception {
         System.out.println("received panic event.........."+event.altitude+"  "+event.latitude+"  "+event.originId);
-            try {
-                //LocationObject lo = new LocationObject(event.latitude, event.longitude);
-                //ObjectMapper mapper = new ObjectMapper();
-                //String json = mapper.writeValueAsString(lo);
-                String json="{ \"title\":\""+ event.originId+"\" , \"position\":{ \"lat\": "+Double.parseDouble(event.latitude)+", \"lng\": "+Double.parseDouble(event.longitude)+" }}";
-               // json="\"{ lat: -25.363, lng: 131.044 }\"";
-                sink.emitNext(json, Sinks.EmitFailureHandler.FAIL_FAST);
-            }catch (Exception e){
-                e.printStackTrace();
+        try {
+            String name="";
+            if("0000fef3-0000-1000-8000-00805f9b34fb".equals(event.originId)){
+                name="Mani E R";
+            }else if("BC:57:29:00:B8:F6".equals(event.originId)){
+                name="Arun";
+            }else{
+                name=event.originId;
             }
+            //LocationObject lo = new LocationObject(event.latitude, event.longitude);
+            //ObjectMapper mapper = new ObjectMapper();
+            //String json = mapper.writeValueAsString(lo);
+            try {
+                String json = "{ \"title\":\"" + name + "\" , \"position\":{ \"lat\": " + Double.parseDouble(event.latitude) + ", \"lng\": " + Double.parseDouble(event.longitude) + " }}";
+                // json="\"{ lat: -25.363, lng: 131.044 }\"";
+
+                try{
+                    sink.emitNext(json, Sinks.EmitFailureHandler.busyLooping(Duration.ofMillis(100)));
+                }catch (Exception ee){
+                    //
+                }
+            }catch (Exception e){
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return ResponseEntity.ok(true);
     }
 
